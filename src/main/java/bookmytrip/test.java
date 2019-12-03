84077302318b99a2192a54280938aff158e76aac
Gaini Zhulamanova <gaini.zhulamanova@gmail.com>
2019-12-03 14:34:39 +0100

    Initial commit

---
+++ src/main/java/bookmytrip/test.java
@@ -0,0 +1,5 @@
+package bookmytrip;
+
+public class test {
+
+}
---
+++ src/test/java/bookmytrip/BookMyTripApplicationTests.java
@@ -0,0 +1,13 @@
+package bookmytrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyTripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
