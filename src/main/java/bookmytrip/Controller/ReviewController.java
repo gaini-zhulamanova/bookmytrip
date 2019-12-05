d15dbfd93235c3b50432cbee3fd7619293a130cf
Yuliya Sheludyakova <julia.sheludyakova@gmail.com>
2019-12-05 17:03:25 +0100

    Queries deleted...lambdas instead

--- src/main/java/bookmytrip/Controller/ReviewController.java
+++ src/main/java/bookmytrip/Controller/ReviewController.java
@@ -2,12 +2,17 @@ package bookmytrip.Controller;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 
+import bookmytrip.Entity.Entry;
+import bookmytrip.Entity.Hotel;
+import bookmytrip.Entity.Museum;
+import bookmytrip.Entity.Restaurant;
 import bookmytrip.Entity.Review;
 import bookmytrip.Repository.ReviewRepository;
 import lombok.RequiredArgsConstructor;
@@ -16,53 +21,64 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @RequestMapping("/book-my-trip/{city}/{entries}/{entry_id}/reviews")
 public class ReviewController {
-	
+
 	// TODO: create tests
 
 	private final ReviewRepository reviewRepo;
-	
+
 	@GetMapping
-	public List<Review> index(@PathVariable String city, @PathVariable String entries, @PathVariable Long entry_id) {
+	public List<Review> index(@PathVariable String city, @PathVariable String entries,
+			@PathVariable Long entry_id) {
+		
+		List<Review> foundReviews = reviewRepo.findAllByCityAndEntryId(city, entry_id);
+		
 		switch (entries) {
-			case "restaurants":
-				return reviewRepo.findAllByRestaurantId(city, entry_id);
-			case "hotels":
-				return reviewRepo.findAllByHotelId(city, entry_id);
-			case "museums":
-				return reviewRepo.findAllByMuseumId(city, entry_id);
-			default:
-				return null;
-		}		
+		case "restaurants":
+			return foundReviews.stream()
+					.filter(r -> r.getEntry() instanceof Restaurant)
+					.collect(Collectors.toList());
+		case "hotels":
+			return foundReviews.stream()
+					.filter(r -> r.getEntry() instanceof Hotel)
+					.collect(Collectors.toList());
+		case "museums":
+			return foundReviews.stream()
+					.filter(r -> r.getEntry() instanceof Museum)
+					.collect(Collectors.toList());
+		default:
+			return null;
+		}
+		
 	}
-	
+
 	@GetMapping("/{id}")
 	public ResponseEntity<?> show(@PathVariable Long id) {
 		Optional<Review> maybeReview = reviewRepo.findById(id);
 		return ResponseEntity.of(maybeReview);
 	}
-	
+
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public Review create(@RequestBody @Valid Review review) {
 		review.setId(null);
 		return reviewRepo.save(review);
-	}	
-	
+	}
+
 	@PutMapping("/{id}")
 	public ResponseEntity<?> update(@PathVariable Long id, @RequestBody Review review) {
-		review.setId(null);		
+		review.setId(null);
 		if (!reviewRepo.existsById(id)) {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-		}		
+		}
 		reviewRepo.save(review);
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
-	
+
 	@DeleteMapping("/{id}")
-	public ResponseEntity<?> delete(@PathVariable Long id) {		
+	public ResponseEntity<?> delete(@PathVariable Long id) {
 		if (!reviewRepo.existsById(id)) {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-		}		
+		}
 		reviewRepo.deleteById(id);
 		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
--- src/main/java/bookmytrip/Repository/ReviewRepository.java
+++ src/main/java/bookmytrip/Repository/ReviewRepository.java
@@ -1,27 +1,40 @@
 package bookmytrip.Repository;
 
-
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
-import bookmytrip.Entity.Review;
+import bookmytrip.Entity.*;
 
 public interface ReviewRepository extends JpaRepository<Review, Long>{
 	
-	@Query(value = "SELECT * FROM Restaurant rest JOIN Entry e ON rest.restaurant_id = e.id JOIN Review r ON r.entry_id = e.id "
-			+ "WHERE e.city = ?1 AND rest.restaurant_id = ?2",
-			nativeQuery = true)
-	List<Review> findAllByRestaurantId(String city, Long id);
+	default public List<Review> findAllByCityAndEntryId(String city, Long entry_id) {
+		return findAll().stream()
+				.filter(r -> r.getEntry().getCity().equals(city))
+				.filter(r -> r.getEntry().getId().equals(entry_id))
+				.collect(Collectors.toList());
+	}
 	
-	@Query(value = "SELECT * FROM Hotel h JOIN Entry e ON h.hotel_id = e.id JOIN Review r ON r.entry_id = e.id "
-			+ "WHERE e.city = ?1 AND h.hotel_id = ?2",
-			nativeQuery = true)
-	List<Review> findAllByHotelId(String city, Long id);
+//	@Query(value = "SELECT * FROM Restaurant rest "
+//			+ "JOIN Entry e ON rest.restaurant_id = e.id "
+//			+ "JOIN Review r ON r.entry_id = e.id "
+//			+ "WHERE e.city = ?1 AND rest.restaurant_id = ?2",
+//			nativeQuery = true)
+//	List<Review> findAllByRestaurantId(String city, Long id);
+//	
+//	@Query(value = "SELECT * FROM Hotel h "
+//			+ "JOIN Entry e ON h.hotel_id = e.id "
+//			+ "JOIN Review r ON r.entry_id = e.id "
+//			+ "WHERE e.city = ?1 AND h.hotel_id = ?2",
+//			nativeQuery = true)
+//	List<Review> findAllByHotelId(String city, Long id);
+//	
+//	@Query(value = "SELECT * FROM Museum m "
+//			+ "JOIN Entry e ON m.museum_id = e.id "
+//			+ "JOIN Review r ON r.entry_id = e.id "
+//			+ "WHERE e.city = ?1 AND m.museum_id = ?2",
+//			nativeQuery = true)
+//	List<Review> findAllByMuseumId(String city, Long id);
 	
-	@Query(value = "SELECT * FROM Museum m JOIN Entry e ON m.museum_id = e.id JOIN Review r ON r.entry_id = e.id "
-			+ "WHERE e.city = ?1 AND m.museum_id = ?2",
-			nativeQuery = true)
-	List<Review> findAllByMuseumId(String city, Long id);
 }
