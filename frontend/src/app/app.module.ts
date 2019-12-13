c86fa275f0cd5ac473e3f1864af2da9eb7beb126
Gaini Zhulamanova <gaini.zhulamanova@gmail.com>
2019-12-13 10:07:12 +0100

    added sidebar component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,12 +6,14 @@ import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
 import { RestaurantComponent } from './restaurant/restaurant.component';
 import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RestaurantComponent,
-    HeaderComponent
+    HeaderComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule, HttpClientModule, FormsModule],
