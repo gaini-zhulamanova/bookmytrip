58aea243b052d93868ba29915aa46d27a92f562f
Gaini Zhulamanova <gaini.zhulamanova@gmail.com>
2019-12-24 17:03:49 +0100

    styled buttons, search field, header, footer, sidebar

--- frontend/src/app/sidebar-filter/sidebar/sidebar.component.ts
+++ frontend/src/app/sidebar-filter/sidebar/sidebar.component.ts
@@ -27,6 +27,7 @@ export class SidebarComponent implements OnInit {
     
     this.initiliseFilter();
   }
+  
 
   initiliseFilter() {
     if(this.entries === 'restaurants' || this.entries === 'museen') {
