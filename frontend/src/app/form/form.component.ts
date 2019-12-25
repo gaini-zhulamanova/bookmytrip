8955a7cb1229adba6e0d1101496625200ffd4df8
unknown <Siddika Celik>
2019-12-25 23:53:18 +0100

    review-form prepped

--- frontend/src/app/form/form.component.ts
+++ frontend/src/app/form/form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-form',
@@ -9,14 +10,32 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class FormComponent implements OnInit {
 
+  city: string;
+  entries: string;
+  entryId: string;
+  reviewId: string;
 
-  constructor(private modalService: NgbModal) { }
-
-  openVerticallyCentered(content) {
-    this.modalService.open(content, { centered: true });
-  }
+  constructor(private modalService: NgbModal,
+              private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit() {
+    this.city = this.route.snapshot.params.city;
+    this.entries = this.route.snapshot.params.entries;
+    this.entryId = this.route.snapshot.params.id;
+    this.reviewId = this.route.snapshot.params.reviewId;
+  }
+
+  openVerticallyCentered(content) {
+    this.modalService.open(content, {centered: true});
+  }
+
+  cancel() {
+    this.router.navigate(['book-my-trip', this.city, this.entries, this.entryId])
+  }
+
+  submit() {
+
   }
 
 }
--- frontend/src/app/form/star-rating/star-rating.component.ts
+++ frontend/src/app/form/star-rating/star-rating.component.ts
@@ -5,7 +5,7 @@ import {NgbRatingConfig} from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-star-rating',
   templateUrl: './star-rating.component.html',
-  styleUrls: ['./star-rating.component.css']
+  styleUrls: ['./star-rating.component.css'],
 })
 export class StarRatingComponent implements OnInit {
 
