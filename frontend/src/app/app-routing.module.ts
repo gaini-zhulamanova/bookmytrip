6e13e73a0bdfe8a2f98160d5cc158ed10d39c0c8
unknown <Siddika Celik>
2019-12-25 20:50:18 +0100

    minor changes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { SidebarFilterComponent } from './sidebar-filter/sidebar-filter.component';
 import { EntryItemComponent } from './entry-item/entry-item.component';
+import { FormComponent } from './form/form.component';
 
 const routes: Routes = [
   {path: "", redirectTo: "/book-my-trip", pathMatch: "full"},
   {path: "book-my-trip", component: HomepageComponent},
   {path: "book-my-trip/:city/:entries", component: SidebarFilterComponent},
-  {path: "book-my-trip/:city/:entries/:id", component: EntryItemComponent}
+  {path: "book-my-trip/:city/:entries/:id", component: EntryItemComponent},
+  {path: "book-my-trip/:city/:entries/:id/bewertungen/neu", component: FormComponent},
+  {path: "book-my-trip/:city/:entries/:id/bewertungen/:reviewId/editieren", component: FormComponent}
 ];
 
 @NgModule({
