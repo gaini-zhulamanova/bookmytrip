0fbf7aef3b64b6848800970b7878e18a8a309567
Gaini Zhulamanova <gaini.zhulamanova@gmail.com>
2019-12-28 18:58:40 +0100

    added dynamic values to entry-item (name, address, reviews), added buttons to entry-item page and to the comment cards, fixed styling of entry name, added functionality to back-button on the entry-item page

--- frontend/src/app/entry-item/comment/comment.component.ts
+++ frontend/src/app/entry-item/comment/comment.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {NgbRatingConfig} from '@ng-bootstrap/ng-bootstrap';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ReviewService } from 'src/app/service/review.service';
+import { Review } from 'src/app/entity/review';
 
 @Component({
   selector: 'app-comment',
@@ -8,12 +11,51 @@ import {NgbRatingConfig} from '@ng-bootstrap/ng-bootstrap';
 })
 export class CommentComponent implements OnInit {
 
-  constructor(config: NgbRatingConfig) {
+  city: string;
+  entry: any;
+  entries: string;
+  entryId: number;
+  id: number;
+
+  reviews: Review[];
+  rating: string;
+  reviewerName: string;
+  reviewTitle: string;
+  comment: string;
+  dateTime: string;
+
+  isMouseOverIcon: boolean;
+
+  constructor(config: NgbRatingConfig,
+              private router: Router,
+              private route: ActivatedRoute,
+              private reviewService: ReviewService) {
     config.max = 5;
     config.readonly = true;
   }
 
   ngOnInit() {
+
+    this.city = this.route.snapshot.params.city;
+    this.entries = this.route.snapshot.params.entries;
+    this.id = this.route.snapshot.params.id;
+    this.entryId = this.route.snapshot.params.entryId;
+
+    this.reviewService.getAll(this.city, this.entries, this.entryId)
+      .subscribe(reviews => this.reviews = reviews);
+  }
+  
+  toEntryForm(id: number) {
+    this.router.navigate(['book-my-trip' + this.city + this.entries + this.id + '/' + 'bewertungen/' + 'neu']);
   }
 
+  onDelete(id: number) {
+    console.log("Nun soll das Element gelöscht werden, vorher aber vllt Fragen ob man sich sicher ist?");
+    // this.entryService.delete(this.city, this.entriesURL, id)
+    //   .subscribe(_ => window.location.reload());
+  }
+
+  isIcon(isMouseOverIcon: boolean) {
+    this.isMouseOverIcon = isMouseOverIcon;
+  }
 }
--- frontend/src/app/entry-item/entry-item.component.ts
+++ frontend/src/app/entry-item/entry-item.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ReviewService } from '../service/review.service';
+import { EntryService } from '../service/entry.service';
+import { Review } from '../entity/review';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-entry-item',
@@ -7,9 +12,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EntryItemComponent implements OnInit {
 
-  constructor() { }
+  city: string;
+  entries: string;
+  id: number;
+  entry: any;
+  reviews: Review[];
+  entriesURL: string;
+
+  constructor(private entryService: EntryService,
+              private reviewService: ReviewService,
+              private route: ActivatedRoute,
+              private router: Router,
+              private location: Location) { }
 
   ngOnInit() {
+
+    this.city = this.route.snapshot.params.city;
+    this.entries = this.route.snapshot.params.entries;
+    this.id = this.route.snapshot.params.id;
+    this.entriesURL = this.route.snapshot.params.entries;
+
+    this.entryService.showById(this.city, this.entries, this.id)
+      .subscribe(entry => this.entry = entry);
+
+    this.reviewService.getAll(this.city, this.entries, this.id)
+      .subscribe(reviews => this.reviews = reviews);
+
   }
 
+  entryTypeMatches(entryType: string): boolean {
+    return this.entriesURL === entryType;
+  }
+
+  toEntryForm(id: number) {
+    this.router.navigate(['book-my-trip' + this.city + this.entries + this.id + '/' + 'bewertungen/' + 'neu']);
+  }
+
+  handleBackButton(){
+    this.location.back();
+  }
 }
